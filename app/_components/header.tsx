5159d769010e511357fd44a403d05cbdc1814c6b
Breno Ribeiro <brenoribeiro.dev@gmail.com>
2024-08-17 00:26:32 -0300

    fix: fix logo display in header on production

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -5,12 +5,13 @@ import { MenuIcon } from "lucide-react"
 import { Sheet, SheetTrigger } from "./ui/sheet"
 import SidebarSheet from "./sidebar-sheet"
 import Link from "next/link"
+
 const Header = () => {
   return (
     <Card className="rounded-none">
       <CardContent className="flex flex-row items-center justify-between p-5">
         <Link href="/">
-          <Image alt="FSW Barber" src="/logo.svg" height={120} width={120} />
+          <Image alt="FSW Barber" src="./logo.svg" height={120} width={120} />
         </Link>
         <Sheet>
           <SheetTrigger asChild>
